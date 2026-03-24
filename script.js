c96d1a4737df89d144a2e0e4dc3423a93c00acb1
defcarlosasnp <87336607+defcarlosasnp@users.noreply.github.com>
2026-03-24 16:46:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-const toggleDark = document.getElementById('toggle-dark');
-toggleDark.addEventListener('click', () => {
-    document.body.classList.toggle('dark');
-    const isDark = document.body.classList.contains('dark');
-    toggleDark.innerHTML = isDark ? '<i data-lucide="sun"></i>' : '<i data-lucide="moon"></i>';
-    lucide.createIcons(); // Recargar iconos
-});
 document.addEventListener('DOMContentLoaded', () => {
     // Inicializar iconos
     lucide.createIcons();
@@ -40,5 +33,4 @@ document.addEventListener('DOMContentLoaded', () => {
             window.open(url, '_blank');
         });
     }
-});
 });
